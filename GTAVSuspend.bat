1b5d532c1ec7c5618e9385868958cecddc7d1ade
Josef <josef733@hotmail.com>
2020-02-25 01:10:12 +0100

    Added 2 Files
    Script and readme files added

---
+++ GTAVSuspend.bat
@@ -0,0 +1,10 @@
+suspend GTA5
+@echo.
+@echo GTAV Paused for 9 seconds. 
+timeout 9 /NOBREAK
+suspend -r GTA5
+@echo.
+@echo GTAV Resumed.
+@echo.
+@echo CMD will close in 5 seconds...
+@timeout 5 > NUL
